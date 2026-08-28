352481216d03ce4b8b28b69e984e5d9431b96458
Waed Mohamed <waed.mohamed@auroraer.com>
2026-08-28 11:54:50 +0100

    deploy: substrate hub 8cd70b6

--- _next/static/chunks/app/(hub)/patterns/forms/page-d2030b957d9188fd.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[144,615,723],{2191:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,2619,23))}},e=>{e.O(0,[619,441,255,358],()=>e(e.s=2191)),_N_E=e.O()}]);
